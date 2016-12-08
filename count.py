29ffcc095a379cfc71394830a7560bd7d49820de
xuefliang <xuefliang@gmail.com>
2016-12-08 22:43:31 +0800

    add count

---
+++ count.py
@@ -0,0 +1,38 @@
+import json
+
+fr = open("data/xyj.txt", "r")
+
+characters = []
+stat = {}
+
+for line in fr:
+	line = line.split()
+
+	if len(line) == 0:
+		continue
+
+	# print type(line)
+
+	temp = json.dumps(line, encoding="UTF-8", ensure_ascii=False)
+	result = [t for t in temp][2:-2]
+
+	for x in xrange(0, len(result)):
+		if not result[x] in characters:
+			characters.append(result[x])
+
+		if not stat.has_key(result[x]):
+			stat[result[x]]=0
+		stat[result[x]]+=1
+fr.close()
+
+stat=sorted(stat.iteritems(),key=lambda d:d[1],reverse=True)
+
+for x in xrange(0,20):
+	print characters[x]
+
+print '*' *20
+
+for x in xrange(0,20):
+	print stat[x][0],stat[x][1]
+
+
