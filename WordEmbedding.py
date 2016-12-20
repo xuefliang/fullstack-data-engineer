a4a1b1e5363e022b6130e29bc3046b31ed181bb7
xuefliang <xuefliang@gmail.com>
2016-12-20 23:06:22 +0800

    word

--- WordEmbedding.py
+++ WordEmbedding.py
@@ -9,7 +9,7 @@ from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 
 # 训练模型
-sentences=LineSentence('wiki.zh.word.text')
+sentences=LineSentence('/home/xuefliang/Downloads/wiki.zh.txt')
 model=Word2Vec(sentences,size=128,window=5,min_count=5,workers=4)
 
 # 保存模型
